28bf2950f2c9b9afdfd0a66b7dd06b4e55e4c1d2
Sriharsha Vogeti <svogeti@paypal.com>
2021-12-03 23:48:57 +0530

    Removing hyperopt

--- gators/model_building/__init__.py
+++ gators/model_building/__init__.py
@@ -1,4 +1,3 @@
-from .hyperopt import HyperOpt
 from .lgbm_treelite_dumper import LGBMTreeliteDumper
 from .train_test_split import TrainTestSplit
 from .xgb_booster_builder import XGBBoosterBuilder
@@ -6,7 +5,6 @@ from .xgb_treelite_dumper import XGBTreeliteDumper
 
 __all__ = [
     "TrainTestSplit",
-    "HyperOpt",
     "XGBBoosterBuilder",
     "XGBTreeliteDumper",
     "LGBMTreeliteDumper",
